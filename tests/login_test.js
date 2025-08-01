1e30c9fd04e39948a64dbfa06688581da01b669b
dhrumilpatel <dhrumilpatel7866@gmail.com>
2025-08-01 11:20:50 +0530

    add place order1

--- tests/login_test.js
+++ tests/login_test.js
@@ -8,36 +8,36 @@ const assert = require('assert');
 
 Feature('Login Page');
 
-// Scenario('Valid sign up flow', async ({ I }) => {
-//   I.amOnPage('/login');
-//   await SignUpPage.signUpForm();
-//   await loginPage.login();
-//   await loginPage.seeWelcomeHeading();
-//   await SearchPage.searchProduct();
-//   await AddToCartPage.add();
-//   await PlaceOrder.place();
-//   await loginPage.seeWelcomeHeading();
-
-// });
-
-Scenario('Invalid login flow', async ({ I }) => {
+Scenario('Valid sign up flow', async ({ I }) => {
   I.amOnPage('/login');
-  // await loginPage.negativeLogin();
-//   await loginPage.loginWithEmail();
+  await SignUpPage.signUpForm();
+  await loginPage.login();
+  await loginPage.seeWelcomeHeading();
+  await SearchPage.searchProduct();
+  await AddToCartPage.add();
+  await PlaceOrder.place();
+  await loginPage.seeWelcomeHeading();
+
+});
+
+// Scenario('Invalid login flow', async ({ I }) => {
+//   I.amOnPage('/login');
+//   // await loginPage.negativeLogin();
+// //   await loginPage.loginWithEmail();
+
+// //   const message = await I.executeScript(() => {
+// //   return document.querySelector('input[type="password"]').validationMessage;
+// // });
+// //   assert.strictEqual(message, 'Please fill in this field.');
+
+//   await loginPage.loginWithPassword
 
 //   const message = await I.executeScript(() => {
 //   return document.querySelector('input[type="password"]').validationMessage;
 // });
 //   assert.strictEqual(message, 'Please fill in this field.');
-
-  await loginPage.loginWithPassword
-
-  const message = await I.executeScript(() => {
-  return document.querySelector('input[type="password"]').validationMessage;
-});
-  assert.strictEqual(message, 'Please fill in this field.');
-  //
-});
+//   //
+// });
 
 
 
